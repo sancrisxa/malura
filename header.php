727c3413a48b494730df51c6dca86e75920dbd0e
sancrisxa <sancrisxa>
2018-11-25 21:03:53 -0200

    adicionado os posts

---
+++ header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    <?php $home = get_template_directory_uri();?>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="<?= $home;?>/reset.css">
+    <link rel="stylesheet" href="<?= $home;?>/style.css">
+    <?php wp_head();?>
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,12 +1,26 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-<body>
+<?php get_header();?>
+    
+<main class="home-main">
+    <div class="container">
+        <h1>Bem Vindo ao Maluras!</h1>
+        <ul class="imoveis-listagem">
+            <?php 
+                if ( have_posts() ) {
+                    while ( have_posts() ) {
+                        the_post(); 
+            ?>
+                        <li class="imoveis-listagem-item">
+                            <?php the_post_thumbnail(); ?>
+                            <h2><?php the_title(); ?></h2>
+                            <p><?php the_content(); ?></p>
+                        </li>
 
-    <h1>Bem vindo!</h1>
+            <?php
+                    } // end while
+                } // end if
+            ?>
+        </ul>
+    </div>
+</main>
 
-</body>
-
-</head>
-</html>
+<?php get_footer();?>
